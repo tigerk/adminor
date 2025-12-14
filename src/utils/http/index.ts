c1345549acfdf814a9cd96dacc2b95e7dc3d0309
tigerk <kimhwawoon@gmail.com>
2025-12-14 16:36:53 +0800

    Update token refresh URL in HTTP white list

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -68,7 +68,7 @@ class PureHttp {
           return config;
         }
         /** 请求白名单，放置一些不需要`token`的接口（通过设置请求白名单，防止`token`过期后再请求造成的死循环问题） */
-        const whiteList = ["/admin/token/refresh", "/login"];
+        const whiteList = ["/saas/token/refresh", "/login"];
         return whiteList.some(url => config.url.endsWith(url))
           ? config
           : new Promise(resolve => {
