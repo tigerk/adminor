be3fa075f632f9e0e49143f39df4f8709658ff29
tigerk <kimhwawoon@gmail.com>
2026-01-04 17:38:27 +0800

    Refactor async routes and update main content margins
    - Refactor `asyncRoutes.ts` to use a new structure for defining mock routes
    - Update the main content margin to 12px in `operation.vue` and `login.vue`

--- mock/asyncRoutes.ts
+++ mock/asyncRoutes.ts
@@ -1,13 +1,5 @@
 // 模拟后端动态生成路由
 import { defineFakeRoute } from "vite-plugin-fake-server/client";
-import {
-  tabs,
-  frame,
-  tenant,
-  system,
-  monitor,
-  permission
-} from "@/router/enums";
 
 /**
  * roles：页面级别权限，这里模拟二种 "admin"、"common"
@@ -15,405 +7,302 @@ import {
  * common：普通角色
  */
 
-const companyManagementRouter = {
-  path: "/platform",
-  name: "Platform",
-  meta: {
-    title: "menus.platformManagement",
-    icon: "ri:settings-3-line"
-  },
-  children: [
-    {
-      path: "/platform/company",
-      name: "PlatformCompany",
-      meta: {
-        icon: "ri:home-gear-line",
-        title: "menus.pureTenantManagement",
-        extraIcon: "IF-pure-iconfont-new svg",
-        showParent: true
+const mockRouter = [
+  {
+    path: "/house",
+    name: "House",
+    redirect: "/house/focus/room",
+    component: null,
+    type: 0,
+    meta: {
+      title: "房源管理",
+      icon: "ep:house",
+      showLink: true,
+      showParent: true,
+      sortOrder: 1,
+      roles: null,
+      auths: [""],
+      keepAlive: false,
+      frameSrc: "",
+      frameLoading: true
+    },
+    children: [
+      {
+        path: "/house/focus",
+        name: "HouseFocus",
+        redirect: null,
+        component: null,
+        type: 0,
+        meta: {
+          title: "集中式",
+          icon: "ri:building-line",
+          showLink: true,
+          showParent: false,
+          sortOrder: 1,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
+        },
+        children: [
+          {
+            path: "/house/focus/room",
+            name: "HouseFocusRoom",
+            redirect: null,
+            component: "/house/focus/focusRoom/index",
+            type: 0,
+            meta: {
+              title: "房间列表",
+              icon: "ri:door-closed-line",
+              showLink: true,
+              showParent: true,
+              sortOrder: 1,
+              roles: null,
+              auths: [""],
+              keepAlive: false,
+              frameSrc: "",
+              frameLoading: true
+            },
+            children: []
+          },
+          {
+            path: "/house/focus/house",
+            name: "HouseFocusHouse",
+            redirect: null,
+            component: "/house/focus/focusHouse/index",
+            type: 0,
+            meta: {
+              title: "项目列表",
+              icon: "ri:profile-line",
+              showLink: true,
+              showParent: true,
+              sortOrder: 2,
+              roles: null,
+              auths: [""],
+              keepAlive: false,
+              frameSrc: "",
+              frameLoading: true
+            },
+            children: []
+          }
+        ]
       },
-      children: [
-        {
-          path: "/platform/company/list/index",
-          component: "/platform/company/list/index",
-          name: "PlatformCompanyList",
-          meta: {
-            icon: "ri:list-check",
-            title: "menus.pureTenantList",
-            showParent: true
-          }
+      {
+        path: "house/scatter",
+        name: "HouseScatter",
+        redirect: null,
+        component: "house/scatter/scatterRoom/index",
+        type: 0,
+        meta: {
+          title: "整/合租",
+          icon: "ri:door-fill",
+          showLink: true,
+          showParent: true,
+          sortOrder: 2,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
         },
-        {
-          path: "/platform/company/package/index",
-          component: "/platform/company/package/index",
-          name: "PlatformCompanyPackage",
-          meta: {
-            icon: "ri:file-paper-line",
-            title: "menus.pureTenantPackage",
-            showParent: true
-          }
-        }
-      ]
-    },
-    {
-      path: "/platform/menu/index",
-      name: "PlatformMenu",
-      meta: {
-        icon: "ep:menu",
-        title: "menus.menuManagement",
-        roles: ["admin"]
+        children: []
       }
-    },
-    {
-      path: "/platform/dict/index",
-      name: "PlatformDict",
-      meta: {
-        icon: "ri:book-2-line",
-        extraIcon: "IF-pure-iconfont-new svg",
-        title: "menus.dictManagement",
-        roles: ["admin"]
-      }
-    }
-  ]
-};
-
-const systemManagementRouter = {
-  path: "/system",
-  meta: {
-    icon: "ri:settings-3-line",
-    title: "menus.pureSysManagement",
-    rank: system
+    ]
   },
-  children: [
-    {
-      path: "/system/user/index",
-      name: "SystemUser",
-      meta: {
-        icon: "ri:admin-line",
-        title: "menus.pureUser",
-        roles: ["admin"]
-      }
+  {
+    path: "/sys",
+    name: "Sys",
+    redirect: null,
+    component: null,
+    type: 0,
+    meta: {
+      title: "menus.pureSysManagement",
+      icon: "ri:settings-3-line",
+      showLink: true,
+      showParent: true,
+      sortOrder: 98,
+      roles: null,
+      auths: [""],
+      keepAlive: false,
+      frameSrc: "",
+      frameLoading: true
     },
-    {
-      path: "/system/role/index",
-      name: "SystemRole",
-      meta: {
-        icon: "ri:admin-fill",
-        title: "menus.pureRole",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/system/menu/index",
-      name: "SystemMenu",
-      meta: {
-        icon: "ep:menu",
-        title: "menus.pureSystemMenu",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/system/dept/index",
-      name: "SystemDept",
-      meta: {
-        icon: "ri:git-branch-line",
-        title: "menus.pureDept",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/system/dict/index",
-      name: "SystemDict",
-      meta: {
-        icon: "ri:book-2-line",
-        extraIcon: "IF-pure-iconfont-new svg",
-        title: "menus.pureDict",
-        roles: ["admin"]
-      }
-    }
-  ]
-};
-
-const systemMonitorRouter = {
-  path: "/monitor",
-  meta: {
-    icon: "ep:monitor",
-    title: "menus.pureSysMonitor",
-    rank: monitor
-  },
-  children: [
-    {
-      path: "/monitor/online-user",
-      component: "monitor/online/index",
-      name: "OnlineUser",
-      meta: {
-        icon: "ri:user-voice-line",
-        title: "menus.pureOnlineUser",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/monitor/login-logs",
-      component: "monitor/logs/login/index",
-      name: "LoginLog",
-      meta: {
-        icon: "ri:window-line",
-        title: "menus.pureLoginLog",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/monitor/operation-logs",
-      component: "monitor/logs/operation/index",
-      name: "OperationLog",
-      meta: {
-        icon: "ri:history-fill",
-        title: "menus.pureOperationLog",
-        roles: ["admin"]
-      }
-    },
-    {
-      path: "/monitor/system-logs",
-      component: "monitor/logs/system/index",
-      name: "SystemLog",
-      meta: {
-        icon: "ri:file-search-line",
-        title: "menus.pureSystemLog",
-        roles: ["admin"]
-      }
-    }
-  ]
-};
-
-const permissionRouter = {
-  path: "/permission",
-  meta: {
-    title: "menus.purePermission",
-    icon: "ep:lollipop",
-    rank: permission
-  },
-  children: [
-    {
-      path: "/permission/page/index",
-      name: "PermissionPage",
-      meta: {
-        title: "menus.purePermissionPage",
-        roles: ["admin", "common"]
-      }
-    },
-    {
-      path: "/permission/button",
-      meta: {
-        title: "menus.purePermissionButton",
-        roles: ["admin", "common"]
+    children: [
+      {
+        path: "/sys/user/index",
+        name: "SysUser",
+        redirect: null,
+        component: "sys/user/index",
+        type: 0,
+        meta: {
+          title: "menus.pureUser",
+          icon: "ri:admin-line",
+          showLink: true,
+          showParent: true,
+          sortOrder: 1,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
+        },
+        children: []
       },
-      children: [
-        {
-          path: "/permission/button/router",
-          component: "permission/button/index",
-          name: "PermissionButtonRouter",
-          meta: {
-            title: "menus.purePermissionButtonRouter",
-            auths: [
-              "permission:btn:add",
-              "permission:btn:edit",
-              "permission:btn:delete"
-            ]
-          }
+      {
+        path: "/sys/role/index",
+        name: "SysRole",
+        redirect: null,
+        component: "sys/role/index",
+        type: 0,
+        meta: {
+          title: "menus.pureRole",
+          icon: "ri:admin-fill",
+          showLink: true,
+          showParent: true,
+          sortOrder: 1,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
         },
-        {
-          path: "/permission/button/login",
-          component: "permission/button/perms",
-          name: "PermissionButtonLogin",
-          meta: {
-            title: "menus.purePermissionButtonLogin"
-          }
-        }
-      ]
-    }
-  ]
-};
-
-const frameRouter = {
-  path: "/iframe",
-  meta: {
-    icon: "ri:links-fill",
-    title: "menus.pureExternalPage",
-    rank: frame
-  },
-  children: [
-    {
-      path: "/iframe/embedded",
-      meta: {
-        title: "menus.pureEmbeddedDoc"
+        children: []
       },
-      children: [
-        {
-          path: "/iframe/colorhunt",
-          name: "FrameColorHunt",
-          meta: {
-            title: "menus.pureColorHuntDoc",
-            frameSrc: "https://colorhunt.co/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
+      {
+        path: "/sys/dept/index",
+        name: "SysDept",
+        redirect: null,
+        component: "sys/dept/index",
+        type: 0,
+        meta: {
+          title: "menus.pureDept",
+          icon: "ri:git-branch-line",
+          showLink: true,
+          showParent: true,
+          sortOrder: 1,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
         },
-        {
-          path: "/iframe/uigradients",
-          name: "FrameUiGradients",
-          meta: {
-            title: "menus.pureUiGradients",
-            frameSrc: "https://uigradients.com/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/ep",
-          name: "FrameEp",
-          meta: {
-            title: "menus.pureEpDoc",
-            frameSrc: "https://element-plus.org/zh-CN/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/tailwindcss",
-          name: "FrameTailwindcss",
-          meta: {
-            title: "menus.pureTailwindcssDoc",
-            frameSrc: "https://tailwindcss.com/docs/installation",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/vue3",
-          name: "FrameVue",
-          meta: {
-            title: "menus.pureVueDoc",
-            frameSrc: "https://cn.vuejs.org/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/vite",
-          name: "FrameVite",
-          meta: {
-            title: "menus.pureViteDoc",
-            frameSrc: "https://cn.vitejs.dev/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/pinia",
-          name: "FramePinia",
-          meta: {
-            title: "menus.purePiniaDoc",
-            frameSrc: "https://pinia.vuejs.org/zh/index.html",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        },
-        {
-          path: "/iframe/vue-router",
-          name: "FrameRouter",
-          meta: {
-            title: "menus.pureRouterDoc",
-            frameSrc: "https://router.vuejs.org/zh/",
-            keepAlive: true,
-            roles: ["admin", "common"]
-          }
-        }
-      ]
-    },
-    {
-      path: "/iframe/external",
-      meta: {
-        title: "menus.pureExternalDoc"
+        children: []
       },
-      children: [
-        {
-          path: "/external",
-          name: "https://pure-admin.cn/",
-          meta: {
-            title: "menus.pureExternalLink",
-            roles: ["admin", "common"]
-          }
+      {
+        path: "/sys/dict/index",
+        name: "SysDict",
+        redirect: null,
+        component: "sys/dict/index",
+        type: 0,
+        meta: {
+          title: "menus.dictManagement",
+          icon: "ri:book-2-line",
+          showLink: true,
+          showParent: true,
+          sortOrder: 1,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
         },
-        {
-          path: "/pureUtilsLink",
-          name: "https://pure-admin-utils.netlify.app/",
-          meta: {
-            title: "menus.pureUtilsLink",
-            roles: ["admin", "common"]
-          }
-        }
-      ]
-    }
-  ]
-};
-
-const tabsRouter = {
-  path: "/tabs",
-  meta: {
-    icon: "ri:bookmark-2-line",
-    title: "menus.pureTabs",
-    rank: tabs
+        children: []
+      }
+    ]
   },
-  children: [
-    {
-      path: "/tabs/index",
-      name: "Tabs",
-      meta: {
-        title: "menus.pureTabs",
-        roles: ["admin", "common"]
-      }
+  {
+    path: "/monitor",
+    name: "SysMonitor",
+    redirect: null,
+    component: null,
+    type: 0,
+    meta: {
+      title: "menus.pureSysMonitor",
+      icon: "ep:monitor",
+      showLink: true,
+      showParent: true,
+      sortOrder: 99,
+      roles: null,
+      auths: [""],
+      keepAlive: false,
+      frameSrc: "",
+      frameLoading: true
     },
-    // query 传参模式
-    {
-      path: "/tabs/query-detail",
-      name: "TabQueryDetail",
-      meta: {
-        // 不在menu菜单中显示
-        showLink: false,
-        activePath: "/tabs/index",
-        roles: ["admin", "common"]
+    children: [
+      {
+        path: "/monitor/online-user",
+        name: "OnlineUser",
+        redirect: null,
+        component: "monitor/online/index",
+        type: 0,
+        meta: {
+          title: "menus.pureOnlineUser",
+          icon: "ri:user-voice-line",
+          showLink: true,
+          showParent: true,
+          sortOrder: 0,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
+        },
+        children: []
+      },
+      {
+        path: "/monitor/login-logs",
+        name: "LoginLog",
+        redirect: null,
+        component: "monitor/logs/login/index",
+        type: 0,
+        meta: {
+          title: "menus.pureLoginLog",
+          icon: "ri:window-line",
+          showLink: true,
+          showParent: true,
+          sortOrder: 1,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
+        },
+        children: []
+      },
+      {
+        path: "/monitor/operation-logs",
+        name: "OperationLog",
+        redirect: null,
+        component: "monitor/logs/operation/index",
+        type: 0,
+        meta: {
+          title: "menus.pureOperationLog",
+          icon: "ri:history-fill",
+          showLink: true,
+          showParent: true,
+          sortOrder: 2,
+          roles: null,
+          auths: [""],
+          keepAlive: false,
+          frameSrc: "",
+          frameLoading: true
+        },
+        children: []
       }
-    },
-    // params 传参模式
-    {
-      path: "/tabs/params-detail/:id",
-      component: "params-detail",
-      name: "TabParamsDetail",
-      meta: {
-        // 不在menu菜单中显示
-        showLink: false,
-        activePath: "/tabs/index",
-        roles: ["admin", "common"]
-      }
-    }
-  ]
-};
+    ]
+  }
+];
 
 export default defineFakeRoute([
   {
-    url: "/get-async-routes",
+    url: "/api/saas/get-async-routes",
     method: "get",
     response: () => {
       return {
-        success: true,
-        data: [
-          companyManagementRouter,
-          systemManagementRouter,
-          systemMonitorRouter,
-          permissionRouter,
-          frameRouter,
-          tabsRouter
-        ]
+        code: 0,
+        msg: "success",
+        data: mockRouter
       };
     }
   }
