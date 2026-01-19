9c3f1834196aaafd63894a530e81f870450e4ba4
tigerk <kimhwawoon@gmail.com>
2026-01-19 12:26:52 +0800

    Refactor delivery and related interfaces to use camelCase for property names
    - Update `DeliveryProps`, `DeliveryItemProps`, and `DeliveryCreateFormProps` to use camelCase for property names
    - Add `inspectorName` to `DeliveryProps`

--- src/types/models/delivery.ts
+++ src/types/models/delivery.ts
@@ -5,13 +5,14 @@
 /** 交割单信息 */
 export interface DeliveryProps {
   id?: bigint;
-  subject_type?: string;
-  subject_type_id?: bigint;
-  room_id?: bigint;
-  handover_type?: string;
+  subjectType?: string;
+  subjectTypeId?: bigint;
+  roomId?: bigint;
+  handoverType?: string;
   status?: number;
-  handover_date?: Date;
-  inspector_id?: bigint;
+  handoverDate?: Date;
+  inspectorId?: bigint;
+  inspectorName?: string;
   remarks?: string;
   /** 交割单明细 */
   items?: DeliveryItemProps[];
@@ -20,28 +21,28 @@ export interface DeliveryProps {
 /** 交割单项目信息 */
 export interface DeliveryItemProps {
   id?: bigint;
-  delivery_id?: bigint;
+  deliveryId?: bigint;
   category?: string;
-  item_id?: bigint;
-  item_name: string;
-  item_unit?: string;
-  pre_value?: string;
-  current_value: string;
+  itemId?: bigint;
+  itemName: string;
+  itemUnit?: string;
+  preValue?: string;
+  currentValue: string;
   damaged?: number;
   remarks?: string;
-  sort_order?: number;
+  sortOrder?: number;
 }
 
 /** 交割单创建表单数据结构定义 */
 export interface DeliveryCreateFormProps {
   id?: bigint;
-  subject_type?: string;
-  subject_type_id?: bigint;
-  room_id?: bigint;
-  handover_type?: string;
+  subjectType?: string;
+  subjectTypeId?: bigint;
+  roomId?: bigint;
+  handoverType?: string;
   status?: number;
-  handover_date?: Date;
-  inspector_id?: bigint;
+  handoverDate?: Date;
+  inspectorId?: bigint;
   remarks?: string;
   /** 交割单明细 */
   items: DeliveryItemProps[];
