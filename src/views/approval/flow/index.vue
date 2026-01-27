43cbbd4d376d461786e128bae14109c355e7c2b9
tigerk <kimhwawoon@gmail.com>
2026-01-27 08:57:40 +0800

    Update approval flow query and DTO

--- src/views/approval/flow/index.vue
+++ src/views/approval/flow/index.vue
@@ -44,7 +44,7 @@
           :loading="loading"
           :loading-config="{ background: 'transparent' }"
           adaptive
-          :adaptiveConfig="{ offsetBottom: 108 }"
+          :adaptiveConfig="{ offsetBottom: 46 }"
           :data="dataList"
           :size="tableSize as any"
           :columns="columns"
