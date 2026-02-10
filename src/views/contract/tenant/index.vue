bae2e7c2c93cf407ba1c651fcdf244db48ec33d7
tigerk <kimhwawoon@gmail.com>
2026-02-10 15:06:55 +0800

    Adjust offsetBottom in tenant view and update dark theme styles
    This commit updates the `offsetBottom` value in the adaptiveConfig for the tenant view and modifies the dark theme styles to remove borders, padding, and adjust the background color.

--- src/views/contract/tenant/index.vue
+++ src/views/contract/tenant/index.vue
@@ -67,7 +67,7 @@
         :loading="loading"
         :loading-config="{ background: 'transparent' }"
         adaptive
-        :adaptiveConfig="{ offsetBottom: 118 }"
+        :adaptiveConfig="{ offsetBottom: 120 }"
         :data="tenantList"
         :size="tableSize as any"
         :columns="columns"
